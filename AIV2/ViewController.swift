04f3c1e8fe76052f98912baf7c138a61d84ab119
D7703_28 <d7703_28@D7703-28ui-iMac.local>
2018-04-04 12:39:35 +0900

    방향등록

--- AIV2/ViewController.swift
+++ AIV2/ViewController.swift
@@ -9,16 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var count: UILabel!
+    var index = 1
+    // 방향
+    
+    var di = true // true 증가 false는 감소
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        myImageView.image = UIImage(named: "frame1.png")
+        count.text = String(1)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func bt(_ sender: Any) {
+        myImageView.image = UIImage(named:"frame2.png")
+        count.text = String(index)
+        
+        //1~5 5~1 방향확인
+        if index == 1 {
+            di = true
+        }else if index == 5 {
+            di = false
+        }
+        if di == true {
+            index = index + 1
+        }else {
+            index = index - 1
+        }
+        
+        
+        
+//        1~5반복
+//        if index == 5 {
+//            index = 1
+//        }else{
+//            index = index + 1
+//        }
+//
+        myImageView.image = UIImage(named: "frame\(index).png")
+        count.text = String(index)
     }
+    
 
 
 }
